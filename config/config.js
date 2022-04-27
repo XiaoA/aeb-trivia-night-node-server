948896023b1aaf4be0b81224be3f9c92b4fb5adb
Andrew Buckingham <abuckingham99@gmail.com>
2022-04-27 00:18:58 -0700

    Update `config/config.js` to prod database config

--- config/config.js
+++ config/config.js
@@ -16,11 +16,6 @@ require('dotenv').config();
     dialect: 'postgres',
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: 'postgres',
+    "use_env_variable": "DATABASE_URL"
   }
 };
